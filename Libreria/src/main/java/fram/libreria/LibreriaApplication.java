ba19c0c3dce85935516fcb2f72aadbcd1306cd85
Tuldrum <90346628+Tuldrum@users.noreply.github.com>
2022-04-30 16:22:12 -0500

    javadoc

--- Libreria/src/main/java/fram/libreria/LibreriaApplication.java
+++ Libreria/src/main/java/fram/libreria/LibreriaApplication.java
@@ -3,10 +3,19 @@ package fram.libreria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Controlador principal
+ * @author Collazos Escobar David, Andrés David Muñoz 
+ *         dacoes@unicauca.edu.co, andresdmunoz@unicauca.edu.co
+ */
 @SpringBootApplication
 public class LibreriaApplication {
 
-	public static void main(String[] args) {
+    /**
+     * función principal 
+     * @param args
+     */
+    public static void main(String[] args) {
 		SpringApplication.run(LibreriaApplication.class, args);
 	}
 
