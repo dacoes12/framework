baf163530a8ba435e4f49ca32a838094ede3f242
Tuldrum <90346628+Tuldrum@users.noreply.github.com>
2022-04-30 16:58:51 -0500

    paquetes

---
+++ Libreria/src/main/java/fram/libreria/acces/data/x.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fram.libreria.acces.data;
+
+/**
+ *
+ * @author ASUS
+ */
+public class x {
+    
+}
